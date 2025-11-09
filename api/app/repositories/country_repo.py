1f9da830ad36d0697684ce73915738eaaad10cb5
aarushb <Aarush.bhat04@gmail.com>
2025-11-08 21:17:50 -0700

    Fix: Added and edited repo files to confine for new auction system

--- api/app/repositories/country_repo.py
+++ api/app/repositories/country_repo.py
@@ -1,41 +1,31 @@
 from sqlalchemy.orm import Session
-from app.models.database import Country, CountryHolding
+from app.models.database import Country
 from typing import List, Optional
+from uuid import UUID
 
 class CountryRepository:
-    def __init__(self, db: Session):
-        self.db = db
-    
-    def create(self, country_data: dict) -> Country:
-        country = Country(**country_data)
-        self.db.add(country)
-        self.db.commit()
-        self.db.refresh(country)
-        return country
-    
-    def get(self, country_id: str) -> Optional[Country]:
-        return self.db.query(Country).filter(Country.id == country_id).first()
-    
-    def get_all(self) -> List[Country]:
-        return self.db.query(Country).all()
-    
-    def get_holdings(self, country_id: str) -> List[CountryHolding]:
-        return self.db.query(CountryHolding).filter(CountryHolding.country_id == country_id).all()
-    
-    def update_holding(self, country_id: str, resource_id: str, quantity_delta: float):
-        holding = self.db.query(CountryHolding).filter(
-            CountryHolding.country_id == country_id,
-            CountryHolding.resource_id == resource_id
-        ).first()
-        
-        if holding:
-            holding.quantity += quantity_delta
-        else:
-            holding = CountryHolding(
-                country_id=country_id,
-                resource_id=resource_id,
-                quantity=quantity_delta
-            )
-            self.db.add(holding)
-        
-        self.db.commit()
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def create(self, country_data: dict) -> Country:
+        country = Country(**country_data)
+        self.db.add(country)
+        self.db.commit()
+        self.db.refresh(country)
+        return country
+    
+    def get(self, country_id: UUID) -> Optional[Country]:
+        return self.db.query(Country).filter(Country.id == country_id, Country.is_deleted == False).first()
+    
+    def get_all(self) -> List[Country]:
+        return self.db.query(Country).filter(Country.is_deleted == False).all()
+    
+    def get_by_name(self, cname: str) -> Optional[Country]:
+        return self.db.query(Country).filter(Country.cname == cname, Country.is_deleted == False).first()
+    
+    def soft_delete(self, country_id: UUID, deleted_by: str):
+        country = self.get(country_id)
+        if country:
+            country.is_deleted = True
+            country.updated_by = deleted_by
+            self.db.commit()
---
+++ api/app/repositories/country_resource_repo.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from app.models.database import CountryResource
+from typing import List, Optional
+from uuid import UUID
+
+class CountryResourceRepository:
+    def __init__(self, db: Session):
+        self.db = db
+    
+    def create(self, cr_data: dict) -> CountryResource:
+        cr = CountryResource(**cr_data)
+        self.db.add(cr)
+        self.db.commit()
+        self.db.refresh(cr)
+        return cr
+    
+    def get(self, country_id: UUID, resource_id: UUID) -> Optional[CountryResource]:
+        return self.db.query(CountryResource).filter(
+            CountryResource.country_id == country_id,
+            CountryResource.resource_id == resource_id,
+            CountryResource.is_deleted == False
+        ).first()
+    
+    def get_by_country(self, country_id: UUID) -> List[CountryResource]:
+        return self.db.query(CountryResource).filter(
+            CountryResource.country_id == country_id,
+            CountryResource.is_deleted == False
+        ).all()
+    
+    def update_quantity(self, country_id: UUID, resource_id: UUID, quantity_delta: float):
+        cr = self.get(country_id, resource_id)
+        if cr:
+            cr.quantity = (cr.quantity or 0) + quantity_delta
+            self.db.commit()
